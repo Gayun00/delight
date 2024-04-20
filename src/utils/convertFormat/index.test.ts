8ba93402327c40061bd980282d94550fd343d437
Gayun00 <gayunlee11@gmail.com>
2024-04-21 03:24:41 +0900

    Add: 시간 변환 util함수 작성, 금액 변환 함수 수정
    - addDollarSign, formatDateTime
    - test 작성

--- src/utils/convertFormat/index.test.ts
+++ src/utils/convertFormat/index.test.ts
@@ -1,6 +1,22 @@
-import { expect, test } from "vitest";
-import { addDollarSign } from ".";
+import { expect, it, describe } from "vitest";
+import { addDollarSign, formatDateTime } from ".";
 
-test("- 혹은 + 문자열 뒤에 $을 삽입한다", () => {
-  expect(addDollarSign("-300")).toBe("-$300");
+describe("addDollarSign 테스트", () => {
+  it("-1380.95를 -$1380.95로 변환한다", () => {
+    expect(addDollarSign("-1380.95")).toBe("-$1380.95");
+  });
+
+  it("4274.26을 +$4274.26으로 변환한다", () => {
+    expect(addDollarSign("4274.26")).toBe("+$4274.26");
+  });
+});
+
+describe("formatDateTime 테스트", () => {
+  it("2023-07-01T00:14:00Z는 9.14 AM으로 변환된다", () => {
+    expect(formatDateTime("2023-07-01T00:14:00Z")).toBe("9.14 AM");
+  });
+
+  it("2023-07-01T02:42:00Z은 11.42 AM으로 변환된다", () => {
+    expect(formatDateTime("2023-07-01T02:42:00Z")).toBe("11.42 AM");
+  });
 });
--- src/utils/convertFormat/index.ts
+++ src/utils/convertFormat/index.ts
@@ -1,3 +1,13 @@
-export const addDollarSign = (inputStr: string) => {
-  return inputStr.replace(/^([-+])/, "-$");
+import dayjs from "dayjs";
+
+export const addDollarSign = (inputStr: string): string => {
+  if (inputStr.includes("-")) {
+    return inputStr.replace(/^([-+])/, "-$");
+  }
+  return "+$" + inputStr;
+};
+
+export const formatDateTime = (dateTimeStr: string) => {
+  const formattedDateTime = dayjs(dateTimeStr).format("h.mm A");
+  return formattedDateTime;
 };
