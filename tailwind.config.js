2d96137da8736526b17061ff0f291fe9ec026a7f
Gayun00 <gayunlee11@gmail.com>
2024-04-20 22:05:53 +0900

    Chore: 스타일링 변수 및 폰트 설정

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,47 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        md: "8px",
+        lg: "20px",
+      },
+      fontSize: {
+        "2xl": "24px",
+        xl: "18px",
+        lg: "16px",
+        md: "14px",
+        sm: "12px",
+      },
+      fontWeight: {
+        extraBold: "700",
+        bold: "600",
+        semiBold: "500",
+        medium: "400",
+      },
+      lineHeight: {
+        "2xl": "36px",
+        xl: "27px",
+        lg: "24px",
+        md: "22px",
+        sm: "21px",
+        xs: "18px",
+      },
+      colors: {
+        purple: {
+          primary: "#363062",
+        },
+        gray: {
+          primary: "#6B6B6B",
+          secondary: "#363062",
+          third: "#BDBDBD",
+          background: "#F5F5F5",
+        },
+        green: {
+          primary: "#5BDAA4",
+        },
+      },
+    },
   },
   plugins: [],
 };
