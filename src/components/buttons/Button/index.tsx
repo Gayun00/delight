79d50f0cf2842b808037c98bcee7d634f677baa7
Gayun00 <gayunlee11@gmail.com>
2024-04-21 01:39:44 +0900

    Chore: Button 컴포넌트 폴더 이동
    - buttonType으로 type props명 변경
    - 불필요한 코드 삭제

--- src/components/buttons/Button/index.tsx
+++ src/components/buttons/Button/index.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 interface Props {
   children: ReactNode;
   variant?: "primary" | "secondary";
+  buttonType?: "submit" | "button";
 }
 
 const styles = {
@@ -10,13 +11,19 @@ const styles = {
   secondary: "bg-tranparent text-gray-primary",
 };
 
-const Button = ({ children, variant = "primary", ...props }: Props) => {
+const Button = ({
+  children,
+  variant = "primary",
+  buttonType = "button",
+  ...props
+}: Props) => {
   return (
-    <div
+    <button
+      type={buttonType}
       {...props}
       className={`${styles[variant]} px-[17px] flex items-center justify-center h-[34px] rounded-lg font-bold text-lg leading-lg`}>
       <p className="px-[3px]">{children}</p>
-    </div>
+    </button>
   );
 };
 
