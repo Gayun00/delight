58b8e37e879eafbb75524f64e035915864ff2d66
Gayun00 <gayunlee11@gmail.com>
2024-04-21 03:26:03 +0900

    Chore: testing-library, 컴포넌트 테스트 환경 설정
    - testUtils 작성

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from "vite";
+import type { InlineConfig } from "vitest";
+import type { UserConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import svgr from "vite-plugin-svgr";
 import path from "path";
+
+interface VitestConfigExport extends UserConfig {
+  test: InlineConfig;
+}
 
 export default defineConfig({
   plugins: [react(), svgr()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
-});
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./src/utils/testSetup.ts"],
+  },
+} as VitestConfigExport);
