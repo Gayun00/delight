d7684ebc98c6fcbdbdc72e02b9cd7dfe17ff4fb2
Gayun00 <gayunlee11@gmail.com>
2024-04-21 12:47:01 +0900

    Add: BottomNavButton UI 컴포넌트

---
+++ src/components/sheets/BottomNav/BottomNavButton/index.tsx
@@ -0,0 +1,22 @@
+import { FunctionComponent, SVGProps } from "react";
+import Empty from "@/assets/empty.svg?react";
+
+interface Props {
+  Icon: FunctionComponent<SVGProps<SVGSVGElement>>;
+  isSelected: boolean;
+}
+
+const BottomNavButton = ({ Icon, isSelected = false }: Props) => {
+  return (
+    <div className="relative flex flex-col items-center h-[67px]">
+      <div className="h-full flex items-center">
+        {Icon ? <Icon fill={isSelected ? "#363062" : "#BDBDBD"} /> : <Empty />}
+      </div>
+      {isSelected && (
+        <div className="absolute bottom-0 w-[38px] h-[5px] rounded-t-sm bg-purple-primary" />
+      )}
+    </div>
+  );
+};
+
+export default BottomNavButton;
